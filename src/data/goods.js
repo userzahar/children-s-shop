ff0c6429e7edf010e51cefcfeffeb5d4c284e92d
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-15 22:03:03 +0200

    add 2 goods items in data array

---
+++ src/data/goods.js
@@ -0,0 +1,108 @@
+export const goods = [
+    {
+        name: "Ромпер з собаками",
+        id: "id-1",
+        price: 510,
+        photo: [
+            {src:"https://i.ibb.co/Hq1myG9/photo-2023-03-15-19-48-42.jpg", alt:"ромпер"}
+        ],
+        category: ["одяг","для найменших"],
+        description: "Тринитка на флісі. Розмір 62см."
+    },
+
+    {
+        name: "Плюшевий плед",
+        id: "id-2",
+        price: 570,
+        photo: [
+            {src:"https://i.ibb.co/d7NXk3V/photo-2023-03-15-19-48-42-3.jpg", alt:"плед"}, 
+            {src:"https://i.ibb.co/PQYj1ss/photo-2023-03-15-19-48-42-2.jpg", alt:"в'язка зблизька"}
+    ],
+        category: ["текстиль","ковдри", "покривала та пледи"],
+        description: "Дитячий плюшевий плед для немовлят Пуффі із неймовірно м'якої, ніжної гіпоалергенної пряжі. Виріб легкий, ніжний та гарний в експлуатації, не електризується, не линяє. Вашій дитинці під таким пледом буде тепло та затишно"
+    },
+
+    {
+        name: "",
+        id: "id-3",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-4",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-5",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-6",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-7",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-8",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+
+    {
+        name: "",
+        id: "id-9",
+        price: 0,
+        photo: [
+            {src:"", alt:""}, 
+            {src:"", alt:""}
+    ],
+        category: ["",""],
+        description: ""
+    },
+]
