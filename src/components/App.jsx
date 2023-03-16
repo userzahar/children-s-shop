533893f31a58abe9a341f85196f02797abef9056
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-16 21:24:38 +0200

    add basic styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
 export const App = () => {
-  return (<div></div>);
+  return (<div>
+    <ul>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+      <li>Lorem, ipsum.</li>
+    </ul>
+  </div>);
 };
