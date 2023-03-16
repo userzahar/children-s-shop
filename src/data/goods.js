e4937531a140bfbc098f3168e31170d01ee26b77
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-16 18:27:42 +0200

    add 9 items of real goods information

--- src/data/goods.js
+++ src/data/goods.js
@@ -23,86 +23,82 @@ export const goods = [
     },
 
     {
-        name: "",
+        name: "Ортопедична подушка",
         id: "id-3",
-        price: 0,
+        price: 120,
         photo: [
-            {src:"", alt:""}, 
-            {src:"", alt:""}
+            {src:"https://i.ibb.co/9TLhYHF/photo-2023-03-15-19-48-41-3.jpg", alt:""}, 
     ],
-        category: ["",""],
-        description: ""
+        category: [],
+        description: "Під замовлення"
     },
 
     {
-        name: "",
+        name: "Куртка демісезонна бежева",
         id: "id-4",
-        price: 0,
+        price: 650,
         photo: [
-            {src:"", alt:""}, 
-            {src:"", alt:""}
+            {src:"https://i.ibb.co/3r4b00h/photo-2023-03-15-19-48-41-2.jpg", alt:"Куртка демісезонна бежева"}, 
     ],
-        category: ["",""],
-        description: ""
+        category: ["одяг"],
+        description: "Розміри 98, 104, 110, 116. Капюшон змінається"
     },
 
     {
-        name: "",
+        name: "Куртка демісезонна темносиня",
         id: "id-5",
-        price: 0,
+        price: 650,
         photo: [
-            {src:"", alt:""}, 
-            {src:"", alt:""}
+            {src:"https://i.ibb.co/cDtR12T/photo-2023-03-15-19-48-41.jpg", alt:"Куртка демісезонна темносиня"}, 
     ],
-        category: ["",""],
-        description: ""
+        category: ["одяг"],
+        description: "Розміри 98, 104, 110, 116. Капюшон змінається"
     },
 
     {
-        name: "",
+        name: "Комплект чоловічок + шапочка",
         id: "id-6",
-        price: 0,
+        price: 320,
         photo: [
-            {src:"", alt:""}, 
-            {src:"", alt:""}
+            {src:"https://i.ibb.co/C90Hwc2/photo-2023-03-15-19-48-42-4.jpg", alt:""}, 
     ],
-        category: ["",""],
-        description: ""
+        category: [],
+        description: "Розмір 62"
     },
 
     {
-        name: "",
+        name: "Боді з довгим рукавом",
         id: "id-7",
-        price: 0,
+        price: 130,
         photo: [
             {src:"", alt:""}, 
             {src:"", alt:""}
     ],
-        category: ["",""],
-        description: ""
+        category: [],
+        description: "Колір: рожевий синій, зелений. Розмір 86."
     },
 
     {
-        name: "",
+        name: "Пелюшки баєві",
         id: "id-8",
-        price: 0,
+        price: 73,
         photo: [
-            {src:"", alt:""}, 
+            {src:"https://i.ibb.co/MRN9Q1m/photo-2023-03-15-19-48-43-7.jpg", alt:"Пелюшки баєві"}, 
             {src:"", alt:""}
     ],
-        category: ["",""],
-        description: ""
+        category: ["текстиль"],
+        description: "Розмір 80/90"
     },
 
     {
-        name: "",
+        name: "Сенсорні кульки",
         id: "id-9",
-        price: 0,
+        price: 150,
         photo: [
-            {src:"", alt:""}, 
-            {src:"", alt:""}
+            {src:"https://ibb.co/M9114hL", alt:"невалідна картинка"}, 
+            {src:"https://i.ibb.co/V3CCZN8/photo-2023-03-15-19-48-43-6.jpg", alt:"валідна картинка"}
     ],
-        category: ["",""],
+        category: ["Іграшки"],
         description: ""
     },
 ]
