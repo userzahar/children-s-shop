9982f42431488f92647736a1f08cdfa2ea1ae04b
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-23 01:18:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { ProductCard } from "./ProductCard/ProductCard";
+//import { ProductCard } from "./ProductCard/ProductCard";
 import { Footer } from "./footer/Footer";
 
 export const App = () => {
