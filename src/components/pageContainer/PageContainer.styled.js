98dc97073f65a0eaa26c582bd8a2ca3b869881a9
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-23 00:21:12 +0200

    change pageContainer and footer styles

--- src/components/pageContainer/PageContainer.styled.js
+++ src/components/pageContainer/PageContainer.styled.js
@@ -8,11 +8,16 @@ export const PageContainerDiv = styled.div`
     padding-left: 15px;
     padding-right: 15px;
 
+    @media screen and (max-width: 1200px){
+        max-width: 768px;
+    }
+
     @media screen and (max-width: 768px){
-        width: 768px;
+        max-width: 480px;
     }
 
     @media screen and (max-width: 480px){
-        width: 480px;
+        width: 100%;
     }
+    //
 `;
