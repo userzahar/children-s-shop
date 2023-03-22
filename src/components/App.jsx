9df4495ff0fd933f640b4dc0628872a8c6c045a6
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-23 00:05:53 +0200

    remove card from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,6 @@ import { ProductCard } from "./ProductCard/ProductCard";
 
 export const App = () => {
   return (<div>
-        <ProductCard data={{
-        name: "Ромпер з собаками",
-        id: "id-1",
-        price: 510,
-        photo: [
-            {src:"https://i.ibb.co/Hq1myG9/photo-2023-03-15-19-48-42.jpg", alt:"ромпер"}
-        ],
-        category: ["одяг","для найменших"],
-        description: "Тринитка на флісі. Розмір 62см."
-    } } />
+
   </div>);
 };
