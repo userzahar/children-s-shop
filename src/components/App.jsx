1c8b6b5c4e6a991a887efab8adbe643f38278b55
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-23 21:49:23 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
-//import { ProductCard } from "./ProductCard/ProductCard";
+import { ProductCard } from "./ProductCard/ProductCard";
 import { Footer } from "./footer/Footer";
 
 export const App = () => {
   return (<div>
     <Footer />
+    <ProductCard data={{
+        name: "Боді з довгим рукавом",
+        id: "id-7",
+        price: 130,
+        photo: [
+ 
+    ],
+        category: [],
+        description: "Колір: рожевий синій, зелений. Розмір 86."
+    }} />
   </div>);
 };
--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -1,35 +1,34 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const FooterTag = styled.footer`
-    width: 100%;
-    position: fixed;
-    bottom: 0;
-    
-    //коли будуть теми зробити імпорт кольору
-    background-color: rgb(3, 169, 161);
-    //
+  width: 100%;
+  position: fixed;
+  bottom: 0;
 
-    //медіазапити переписати під уніфіковані
-    @media screen and (max-width: 768px){
-        position: static;
-    }
-    //
+  //коли будуть теми зробити імпорт кольору
+  background-color: rgb(3, 169, 161);
+  //
+
+  //медіазапити переписати під уніфіковані
+  @media screen and (max-width: 768px) {
+    position: static;
+  }
+  //
 `;
 
 export const AddressTag = styled.address`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    min-height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 40px;
 
-    //коли будуть теми зробити імпорт кольору
-    color: white;
-    //
+  //коли будуть теми зробити імпорт кольору
+  color: white;
+  //
 
-
-    //медіазапити переписати під уніфіковані
-    @media screen and (max-width: 768px){
-        flex-direction: column;
-    }
-    //
+  //медіазапити переписати під уніфіковані
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+  //
 `;
