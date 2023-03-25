ebde09a3e9028529ac2643cd4431eb24b96ee9f4
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-25 19:56:39 +0200

    add media for footer

--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import { device } from "utils/mediaVariables";
+
 
 export const FooterTag = styled.footer`
   width: 100%;
@@ -9,11 +11,10 @@ export const FooterTag = styled.footer`
   background-color: rgb(3, 169, 161);
   //
 
-  //медіазапити переписати під уніфіковані
-  @media screen and (max-width: 768px) {
+  @media ${device.mobile} {
     position: static;
   }
-  //
+
 `;
 
 export const AddressTag = styled.address`
@@ -21,14 +22,13 @@ export const AddressTag = styled.address`
   justify-content: space-between;
   align-items: center;
   min-height: 40px;
-
+  
   //коли будуть теми зробити імпорт кольору
   color: white;
   //
 
-  //медіазапити переписати під уніфіковані
-  @media screen and (max-width: 768px) {
+  @media ${device.mobile} {
     flex-direction: column;
   }
-  //
+
 `;
