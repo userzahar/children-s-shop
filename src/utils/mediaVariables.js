1becf2b500bad786ec4801d55a14cb7d7a8e8fd7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-26 07:30:09 +0300

    freeze media objects

--- src/utils/mediaVariables.js
+++ src/utils/mediaVariables.js
@@ -1,16 +1,15 @@
-const breakpoints = {
+const breakpoints = Object.freeze({
     mobile: '480px',
     tablet: '768px',
     desktop: '1200px',
-};
+});
 
 //desktop-first
-export const device = {
+export const device = Object.freeze({
     desktop: `screen and min-width: ${breakpoints.desktop}`,
     tablet: `screen and max-width: ${breakpoints.desktop}`,
     onlyTablet: `screen and min-width: ${breakpoints.tablet} and max-width: ${breakpoints.desktop}`,
     biggerThanMobile: `screen and min-width: ${breakpoints.tablet}`,
     mobile: `screen and max-width: ${breakpoints.tablet}`,
     smallerThanMobile: `screen and max-width: ${breakpoints.mobile}`,
-
-};
+});
