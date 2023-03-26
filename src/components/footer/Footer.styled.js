d98bdbf854532b885951b20bfbb24e110adc2cd7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-26 08:34:36 +0300

    rename media queries file

--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { device } from "utils/mediaVariables";
+import { device } from "utils/mediaQueries";
 
 
 export const FooterTag = styled.footer`
--- src/components/pageContainer/PageContainer.styled.js
+++ src/components/pageContainer/PageContainer.styled.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { device } from "utils/mediaVariables";
+import { device } from "utils/mediaQueries";
 
 export const PageContainerDiv = styled.div`
     margin: 0 auto;
