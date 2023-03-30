99d48e339b657b2a9c265a56602c22874ca43d8e
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-31 01:58:39 +0300

    fix bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 
 import { goods } from "data/goods";
 import { Header } from "./Header/Header";
-import { Footer } from "./Footer/Footer";
+import { Footer } from "./footer/Footer";
 import { GalleryProducts } from "./GalleryProducts/GalleryProducts";
 import { Main } from "./Main/Main";
 import { PageContainer } from "./pageContainer/pageContainer";
